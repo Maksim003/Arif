126b3349c4499ccdacff8da723b1241116a81ac3
Maksim003 <savchik00@list.ru>
2021-10-06 20:15:04 +0300

    init(Task#1):Arif

---
+++ Arif/Arif/Arif.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a;
+	cout << "Введите a: ";
+	cin >> a;
+	if (a % 2 == 0)
+	{
+		cout << "a-чётное" << endl;
+	}
+	else
+	{
+		cout << "a-нечётное" << endl;
+	}
+	int b, c, d, e;
+	double arif;
+	b = a % 10;
+	c = a / 100;
+	d = a % 100;
+	e = d / 10;
+	arif = (double)(b + c + e) / 3.;
+	cout << "Среднее арифметическое трёхзначного числа равно =" << arif;
+	return 0;
+
+}
